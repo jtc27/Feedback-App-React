cf15732c4c10ceac09a443ebfff5a7fcff4a4b4f
Jacob Cho <jacobcho@live.com>
2022-03-22 12:49:12 +0000

    commit

---
+++ src backups/App copy with conditions.js
@@ -0,0 +1,38 @@
+function App() {
+
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+    {id: 4, text: 'Comment four'},
+  ]
+
+  const loading = false;
+  const showComments = true;
+
+  const commentBlock = 
+  <div className="comments">
+  <h3>Comments ({comments.length})</h3>
+    <ul>
+    {comments.map((comment, index) => (
+      <li key={comment.id}>{comment.text}</li>
+    ))}
+    </ul>
+  </div>
+
+  if(loading) return <h1>Loading...</h1>
+
+  return (
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock }
+  
+    </div>
+  )
+}
+
+export default App
