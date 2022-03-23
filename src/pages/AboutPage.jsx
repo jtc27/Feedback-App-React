64d3a6988da0d8f84113e957ab94300229536261
Jacob Cho <jacobcho@live.com>
2022-03-23 19:02:42 +0000

    react router v6 created about page created

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from "../components/shared/card"
+
+
+function AboutPage() {
+  return (
+    <Card> 
+      <h1>About this Project</h1>
+      <p>This is a React app to leave feedback for a product</p>
+      <p>Version: 1.0.0</p>
+
+      <p>
+        <a href="/">back to home</a>
+      </p>
+    </Card>
+  )
+}
+
+export default AboutPage
